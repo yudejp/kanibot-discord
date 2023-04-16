f73a157ae54339f3ab8de57c8472e741b7bff93b
iamtakagi <github@iamtakagi.net>
2023-04-16 19:45:10 +0900

    kani

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,6 @@ import tweepy
 import datetime
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-SCREEN_NAME = os.getenv('SCREEN_NAME', 'inu__bot')
+SCREEN_NAME = os.getenv('SCREEN_NAME', 'kani_lyric')
 API = tweepy.API(tweepy.OAuth2BearerHandler(os.getenv('TWITTER_BEARER')))
 STARTED = datetime.datetime.utcnow()
